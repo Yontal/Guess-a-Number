f25a204de2437db279e3343a012d86639102b688
Yontal <eq2@yandex.ru>
2020-03-11 17:01:31 +0300

    added some styling

--- components/Header.js
+++ components/Header.js
@@ -2,11 +2,12 @@ import React from 'react'
 import {View, Text, StyleSheet} from 'react-native'
 
 import COLOR from '../constants/Colors'
+import STYLES from '../constants/default-styles'
 
 const Header = ({title}) => {
     return(
         <View style={styles.header}>
-            <Text style={styles.headerTitle}>{title}</Text>
+            <Text style={[styles.headerTitle, STYLES.titleText]}>{title}</Text>
         </View>
     );
 }
